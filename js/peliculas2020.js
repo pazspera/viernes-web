886a80209ae49bce82504c17ac223cb9e594257f
Paz Spera <mspera03@gmail.com>
2021-07-18 23:28:26 -0300

    agregué pelis faltantes de 2020 y pasé a js las cards de 2020

---
+++ js/peliculas2020.js
@@ -0,0 +1,164 @@
+// Mayo 2020
+// Crear un array con las peliculas de Mayo 2021
+const peliculasMayo20 = peliculasCards.filter(a => a.fecha_vista.getFullYear() == 2020 && a.fecha_vista.getMonth() == 4);
+
+// Ordenar películas, la más vieja primero
+peliculasMayo20.sort((a,b) => a.fecha_vista - b.fecha_vista);
+
+// Printear las cards del subarray de Mayo 2021
+document.getElementById('pelis_mayo_20').innerHTML = `
+${peliculasMayo20.map(function(pelicula_card){
+    return `
+    <div class="card card-pelicula col-12 col-lg-6">
+        <img src="../${pelicula_card.img_card}" alt="${pelicula_card.img_card_alt}" class="card-img-top">
+        <div class="card-body card-pelicula__contenido">
+            <h2 class="card-pelicula__titulo"><a href="${pelicula_card.link_pagina}" class=" stretched-link">${pelicula_card.nombre} (${pelicula_card.anio})</a></h2>
+        </div>
+    </div>
+    `;
+}).join('')}`;
+
+// Junio 2020
+// Crear un array con las peliculas de Mayo 2021
+const peliculasJunio20 = peliculasCards.filter(a => a.fecha_vista.getFullYear() == 2020 && a.fecha_vista.getMonth() == 5);
+
+// Ordenar películas, la más vieja primero
+peliculasJunio20.sort((a,b) => a.fecha_vista - b.fecha_vista);
+
+// Printear las cards del subarray de Mayo 2021
+document.getElementById('pelis_junio_20').innerHTML = `
+${peliculasJunio20.map(function(pelicula_card){
+    return `
+    <div class="card card-pelicula col-12 col-lg-6">
+        <img src="../${pelicula_card.img_card}" alt="${pelicula_card.img_card_alt}" class="card-img-top">
+        <div class="card-body card-pelicula__contenido">
+            <h2 class="card-pelicula__titulo"><a href="${pelicula_card.link_pagina}" class=" stretched-link">${pelicula_card.nombre} (${pelicula_card.anio})</a></h2>
+        </div>
+    </div>
+    `;
+}).join('')}`;
+
+// Julio 2020
+// Crear un array con las peliculas de Mayo 2021
+const peliculasJulio20 = peliculasCards.filter(a => a.fecha_vista.getFullYear() == 2020 && a.fecha_vista.getMonth() == 6);
+
+// Ordenar películas, la más vieja primero
+peliculasJulio20.sort((a,b) => a.fecha_vista - b.fecha_vista);
+
+// Printear las cards del subarray de Mayo 2021
+document.getElementById('pelis_julio_20').innerHTML = `
+${peliculasJulio20.map(function(pelicula_card){
+    return `
+    <div class="card card-pelicula col-12 col-lg-6">
+        <img src="../${pelicula_card.img_card}" alt="${pelicula_card.img_card_alt}" class="card-img-top">
+        <div class="card-body card-pelicula__contenido">
+            <h2 class="card-pelicula__titulo"><a href="${pelicula_card.link_pagina}" class=" stretched-link">${pelicula_card.nombre} (${pelicula_card.anio})</a></h2>
+        </div>
+    </div>
+    `;
+}).join('')}`;
+
+// Agosto 2020
+// Crear un array con las peliculas de Mayo 2021
+const peliculasAgosto20 = peliculasCards.filter(a => a.fecha_vista.getFullYear() == 2020 && a.fecha_vista.getMonth() == 7);
+
+// Ordenar películas, la más vieja primero
+peliculasAgosto20.sort((a,b) => a.fecha_vista - b.fecha_vista);
+
+
+// Printear las cards del subarray de Mayo 2021
+document.getElementById('pelis_agosto_20').innerHTML = `
+${peliculasAgosto20.map(function(pelicula_card){
+    return `
+    <div class="card card-pelicula col-12 col-lg-6">
+        <img src="../${pelicula_card.img_card}" alt="${pelicula_card.img_card_alt}" class="card-img-top">
+        <div class="card-body card-pelicula__contenido">
+            <h2 class="card-pelicula__titulo"><a href="${pelicula_card.link_pagina}" class=" stretched-link">${pelicula_card.nombre} (${pelicula_card.anio})</a></h2>
+        </div>
+    </div>
+    `;
+}).join('')}`;
+
+// Septiembre 2020
+// Crear un array con las peliculas de Mayo 2021
+const peliculasSeptiembre20 = peliculasCards.filter(a => a.fecha_vista.getFullYear() == 2020 && a.fecha_vista.getMonth() == 8);
+
+// Ordenar películas, la más vieja primero
+peliculasSeptiembre20.sort((a,b) => a.fecha_vista - b.fecha_vista);
+
+
+// Printear las cards del subarray de Mayo 2021
+document.getElementById('pelis_septiembre_20').innerHTML = `
+${peliculasSeptiembre20.map(function(pelicula_card){
+    return `
+    <div class="card card-pelicula col-12 col-lg-6">
+        <img src="../${pelicula_card.img_card}" alt="${pelicula_card.img_card_alt}" class="card-img-top">
+        <div class="card-body card-pelicula__contenido">
+            <h2 class="card-pelicula__titulo"><a href="${pelicula_card.link_pagina}" class=" stretched-link">${pelicula_card.nombre} (${pelicula_card.anio})</a></h2>
+        </div>
+    </div>
+    `;
+}).join('')}`;
+
+// Octubre 2020
+// Crear un array con las peliculas de Mayo 2021
+const peliculasOctubre20 = peliculasCards.filter(a => a.fecha_vista.getFullYear() == 2020 && a.fecha_vista.getMonth() == 9);
+
+// Ordenar películas, la más vieja primero
+peliculasOctubre20.sort((a,b) => a.fecha_vista - b.fecha_vista);
+
+
+// Printear las cards del subarray de Mayo 2021
+document.getElementById('pelis_octubre_20').innerHTML = `
+${peliculasOctubre20.map(function(pelicula_card){
+    return `
+    <div class="card card-pelicula col-12 col-lg-6">
+        <img src="../${pelicula_card.img_card}" alt="${pelicula_card.img_card_alt}" class="card-img-top">
+        <div class="card-body card-pelicula__contenido">
+            <h2 class="card-pelicula__titulo"><a href="${pelicula_card.link_pagina}" class=" stretched-link">${pelicula_card.nombre} (${pelicula_card.anio})</a></h2>
+        </div>
+    </div>
+    `;
+}).join('')}`;
+
+// Noviembre 2020
+// Crear un array con las peliculas de Mayo 2021
+const peliculasNoviembre20 = peliculasCards.filter(a => a.fecha_vista.getFullYear() == 2020 && a.fecha_vista.getMonth() == 10);
+
+// Ordenar películas, la más vieja primero
+peliculasNoviembre20.sort((a,b) => a.fecha_vista - b.fecha_vista);
+
+
+// Printear las cards del subarray de Mayo 2021
+document.getElementById('pelis_noviembre_20').innerHTML = `
+${peliculasNoviembre20.map(function(pelicula_card){
+    return `
+    <div class="card card-pelicula col-12 col-lg-6">
+        <img src="../${pelicula_card.img_card}" alt="${pelicula_card.img_card_alt}" class="card-img-top">
+        <div class="card-body card-pelicula__contenido">
+            <h2 class="card-pelicula__titulo"><a href="${pelicula_card.link_pagina}" class=" stretched-link">${pelicula_card.nombre} (${pelicula_card.anio})</a></h2>
+        </div>
+    </div>
+    `;
+}).join('')}`;
+
+// DICIEMBRE 2020
+// Crear un array con las peliculas de Mayo 2021
+const peliculasDiciembre20 = peliculasCards.filter(a => a.fecha_vista.getFullYear() == 2020 && a.fecha_vista.getMonth() == 11);
+
+// Ordenar películas, la más vieja primero
+peliculasDiciembre20.sort((a,b) => a.fecha_vista - b.fecha_vista);
+
+
+// Printear las cards del subarray de Mayo 2021
+document.getElementById('pelis_diciembre_20').innerHTML = `
+${peliculasDiciembre20.map(function(pelicula_card){
+    return `
+    <div class="card card-pelicula col-12 col-lg-6">
+        <img src="../${pelicula_card.img_card}" alt="${pelicula_card.img_card_alt}" class="card-img-top">
+        <div class="card-body card-pelicula__contenido">
+            <h2 class="card-pelicula__titulo"><a href="${pelicula_card.link_pagina}" class=" stretched-link">${pelicula_card.nombre} (${pelicula_card.anio})</a></h2>
+        </div>
+    </div>
+    `;
+}).join('')}`;
