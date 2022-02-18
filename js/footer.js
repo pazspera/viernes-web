6450b134bd443eb5e239ed4aaff5c33a75e07b6d
Paz Spera <mspera03@gmail.com>
2022-02-17 22:37:48 -0300

    agregué rel noreferrer a links footer

--- js/footer.js
+++ js/footer.js
@@ -9,10 +9,10 @@ class Footer extends HTMLElement {
             <div class="container">
                 <div class="row">
                 <div class="col-11">
-                    <p>Sitio desarrollado con <i class="fas fa-heart footer__heart"></i> por <a href="https://github.com/pazspera" class="link-line" target="_blank">Paz Spera</a></p>
+                    <p>Sitio desarrollado con <i class="fas fa-heart footer__heart"></i> por <a href="https://github.com/pazspera" class="link-line" target="_blank" rel="noreferrer">Paz Spera</a></p>
                 </div>
                 <div class="col-1 d-flex justify-content-end">
-                    <a href="https://discord.gg/sHEkMhF" target="_blank"><i class="fab fa-discord"></i></a>
+                    <a href="https://discord.gg/sHEkMhF" target="_blank" rel="noreferrer"><i class="fab fa-discord"></i></a>
                 </div>
                 </div>
             </div>
