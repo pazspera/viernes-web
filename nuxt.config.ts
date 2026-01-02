8d26c7d242ed22d1dca4368c00b74325aa615d02
Paz Spera <mspera03@gmail.com>
2026-01-02 18:36:58 -0300

    chore: Instalado el módulo ESLint con configuración default

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,6 @@ import { defineNuxtConfig } from 'nuxt/config';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxt/eslint']
 })
