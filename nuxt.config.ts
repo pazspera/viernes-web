1362aee99d0683efe2a35525bc5545734d7274c4
Paz Spera <mspera03@gmail.com>
2026-01-13 14:42:48 -0300

    chore: Instalación de @nuxt/fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,11 @@ import { defineNuxtConfig } from 'nuxt/config'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
-  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss'],
+  modules: [
+    '@nuxt/eslint',
+    '@nuxtjs/tailwindcss',
+    '@nuxt/fonts'
+  ],
   devtools: { enabled: true },
   compatibilityDate: '2025-07-15',
   // @ts-expect-error: Genera errores durante CI proque los tipos no se generan todavía cuando verifica el lint
