bfa04451425dbacd1bedabff0151d0473546b6da
Paz Spera <mspera03@gmail.com>
2026-01-14 12:29:07 -0300

    chore: Configuración de nuxt/test-utils con el wizard

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,11 @@
-import { defineNuxtConfig } from 'nuxt/config'
+import { defineNuxtConfig } from "nuxt/config";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    '@nuxt/eslint',
-    '@nuxtjs/tailwindcss',
-    '@nuxt/fonts',
+    "@nuxt/eslint",
+    "@nuxtjs/tailwindcss",
+    "@nuxt/fonts",
+    "@nuxt/test-utils/module",
   ],
   /* fonts: {
     families: [
@@ -13,17 +14,25 @@ export default defineNuxtConfig({
       { name: 'Major Mono Display', weights: [ 400, 700 ] },
     ]
   }, */
-  devtools: { enabled: true }, app: {
+  devtools: { enabled: true },
+  app: {
     head: {
       link: [
-        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
-        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Cardo:wght@700&family=Karla:wght@300;400;700&family=Major+Mono+Display&family=Outfit:wght@400;700&display=swap' },
+        { rel: "preconnect", href: "https://fonts.googleapis.com" },
+        {
+          rel: "preconnect",
+          href: "https://fonts.gstatic.com",
+          crossorigin: "",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Cardo:wght@700&family=Karla:wght@300;400;700&family=Major+Mono+Display&family=Outfit:wght@400;700&display=swap",
+        },
       ],
     },
   },
-  css: ['@/assets/css/main.css'],
-  compatibilityDate: '2025-07-15',
+  css: ["@/assets/css/main.css"],
+  compatibilityDate: "2025-07-15",
   // @ts-expect-error: Genera errores durante CI proque los tipos no se generan todavía cuando verifica el lint
   eslint: {
     config: {
@@ -34,4 +43,4 @@ export default defineNuxtConfig({
     exposeConfig: true,
     viewer: true,
   },
-})
+});
