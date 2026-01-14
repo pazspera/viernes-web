373d05fa781c4c3d472066728c3196e1fd8f60bf
Paz Spera <mspera03@gmail.com>
2026-01-14 14:36:12 -0300

    chore: Normalizado código con lint:fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
-import withNuxt from "./.nuxt/eslint.config.mjs";
+import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt({
   rules: {
-    "no-console": "warn",
-    "vue/multi-word-component-names": "off",
-    "vue/singleline-html-element-content-newline": "off",
-    "vue/multiline-html-element-content-newline": "off",
-    "@stylistic/quotes": ["error", "single"],
-    "@stylistic/semi": ["error", "never"],
-    "@stylistic/comma-dangle": ["error", "always-multiline"],
+    'no-console': 'warn',
+    'vue/multi-word-component-names': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/multiline-html-element-content-newline': 'off',
+    '@stylistic/quotes': ['error', 'single'],
+    '@stylistic/semi': ['error', 'never'],
+    '@stylistic/comma-dangle': ['error', 'always-multiline'],
   },
-});
+})
