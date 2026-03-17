61acc921b203aba0e1271800923448ffb774fa1d
Paz Spera <mspera03@gmail.com>
2026-03-16 23:02:59 -0300

    feature: Creadas páginas para random e historial. Creado componente Navbar con esqueleto inicial

---
+++ app/components/Navbar.vue
@@ -0,0 +1,9 @@
+<template>
+  <header>
+    <nav>
+      <NuxtLink to="/">Viernes</NuxtLink>
+      <NuxtLink to="/historial">Historial</NuxtLink>
+      <NuxtLink to="/random">Random</NuxtLink>
+    </nav>
+  </header>
+</template>
---
+++ app/pages/historial.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Historial de películas</h1>
+</template>
