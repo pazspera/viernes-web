264c7f9f65afbe15bd584cfad8973b79cf30f822
Paz Spera <mspera03@gmail.com>
2026-03-18 00:38:50 -0300

    style: Estilos iniciales de Navbar y links

--- app/components/Navbar.vue
+++ app/components/Navbar.vue
@@ -1,9 +1,11 @@
 <template>
-  <header>
-    <nav>
-      <NuxtLink to="/">Viernes</NuxtLink>
-      <NuxtLink to="/historial">Historial</NuxtLink>
-      <NuxtLink to="/random">Random</NuxtLink>
+  <header class="bg-black">
+    <nav class="container mx-auto py-3 flex justify-between">
+      <NuxtLink to="/" class="font-logo text-white hover:text-primary-500">viernes</NuxtLink>
+      <div>
+        <NuxtLink to="/historial" class="text-white hover:text-primary-500 ps-3">Historial</NuxtLink>
+        <NuxtLink to="/random" class="ps-3">Random</NuxtLink>
+      </div>
     </nav>
   </header>
 </template>
