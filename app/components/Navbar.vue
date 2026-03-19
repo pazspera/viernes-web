5f59d1dfe32e2470f42c6b31ef064432f900e170
Paz Spera <mspera03@gmail.com>
2026-03-18 23:11:50 -0300

    feature: Agregados links para versión mobile

--- app/components/Navbar.vue
+++ app/components/Navbar.vue
@@ -1,14 +1,33 @@
+<script>
+/*
+- crear links para versión mobile y desktop
+- crear contenedor para links en mobile
+*/
+</script>
+
 <template>
   <header class="bg-black">
-    <nav class="container mx-auto py-3 flex justify-between">
+    <nav class="container mx-auto py-3 px-3 flex flex-col md:flex-row justify-between items-center">
       <Logo to="/">
         VieRnes
       </Logo>
-      <div>
+
+      <!-- desktop links -->
+      <div class="hidden md:flex">
         <NavLink to="/historial">
           Historial
         </NavLink>
         <NavLink to="/random" class="ps-3">Random</NavLink>
+      </div>
+
+      <!-- drawer mobile -->
+      <div class="flex w-full bg-black">
+        <div class="flex flex-col p-4">
+          <NavLink to="/historial">
+            Historial
+          </NavLink>
+          <NavLink to="/random" class="ps-3">Random</NavLink>
+        </div>
       </div>
     </nav>
   </header>
