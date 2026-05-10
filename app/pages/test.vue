8a5a7a45d741e955f82d10854ce7246ca3063b48
Paz Spera <mspera03@gmail.com>
2026-05-10 18:14:13 -0300

    refactor: Actualizadas las rutas de imagen en random.json de rutas relativas a rutas absolutas

--- app/pages/test.vue
+++ app/pages/test.vue
@@ -20,9 +20,10 @@ const { data: movies } = await useAsyncData("movies",
     {{ movie.name }} ({{ movie.year }})
   </p>
   <h2>Test random</h2>
-  <p v-for="movie in random?.movies ?? []" :key="movie.id">
-    {{ movie.name }}
-  </p>
+  <div v-for="movie in random?.movies ?? []" :key="movie.id">
+    <p>{{ movie.name }}</p>
+    <img :src="movie.img" />
+  </div>
   <h2>Pelis de viernes</h2>
   <p v-for="movie in movies?.movies ?? []" :key="movie.id">
     {{ movie.name }} ({{ movie.year }}) - Dirección: {{ movie.director }}
