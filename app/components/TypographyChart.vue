5fabdabcd33f1b8708d548fa8157959a9ca82915
Paz Spera <mspera03@gmail.com>
2026-05-24 03:27:57 -0300

    feature: Creado componente TypographyChart para visualizar los componentes de tipografía

---
+++ app/components/TypographyChart.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import SectionTitle from './SectionTitle.vue';
+
+const components = [
+  /* {
+    component: "MainTitle",
+    tag: "h1",
+    description: "Título de las páginas de random, películas individuales e historial",
+    preview: h1
+  }, */
+  {
+    component: "SectionTitle",
+    tag: "h2",
+    description: "Subtítulo dentro de una página. En películas, va en sinopsis, elenco, trailer. En historial, va en cada mes",
+    preview: SectionTitle
+  },
+]
+</script>
+
+<template>
+  <table class="table-auto w-full">
+    <caption>
+      Chart de componentes de tipografía
+    </caption>
+    <thead class="table-header-group">
+      <tr class="table-row">
+        <th class="table-cell" scope="col">Componente</th>
+        <th class="table-cell" scope="col">Etiqueta</th>
+        <th class="table-cell" scope="col">Preview</th>
+        <th class="table-cell" scope="col">Descripción</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr class="table-row" v-for="component in components" :key="component.component">
+        <td class="table-cell" scope="row">{{ component.component }}</td>
+        <td class="table-cell">{{ component.tag }}</td>
+        <td class="table-cell">
+          <component :is="component.preview">{{ component.component }}</component>
+        </td>
+        <td class="table-cell">{{ component.description }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,8 +1,5 @@
 <template>
   <div class="bg-accent-100">
-    <p class="font-logo text-primary-500">contenido de la página principal</p>
-    <p>Un p</p>
-    <h1>Un h1</h1>
-    <SectionTitle>Section Title</SectionTitle>
+    <TypographyChart />
   </div>
 </template>
