97d92b3bdd311c8956bf2a72ff61c9ed66beb077
Paz Spera <mspera03@gmail.com>
2026-05-31 14:57:47 -0300

    feature: Agregado componente ButtonPrimary con estilos

---
+++ app/components/ButtonPrimary.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import type { NuxtLinkProps } from '#app';
+const props = defineProps<NuxtLinkProps>();
+
+defineOptions({ name: "ButtonPrimary" });
+</script>
+
+<template>
+  <NuxtLink v-bind="props"
+    class="button-primary bg-white text-black hover:bg-black hover:text-white border-black border">
+    <ButtonLabel>
+      <slot />
+    </ButtonLabel>
+  </NuxtLink>
+</template>
+
+<style lang="css" scoped>
+.button-primary {
+  transition: all 0.25s ease-in-out;
+}
+
+.button-primary:hover {
+  transition: all 0.25s ease-in-out;
+}
+
+@media (max-width: 600px) {
+  .button-primary {
+    padding: 8px 16px;
+  }
+}
+
+@media (min-width: 601px) and (max-width: 960px) {
+  .button-primary {
+    padding: 10px 20px;
+  }
+}
+
+@media (min-width: 961px) and (max-width: 1280px) {
+  .button-primary {
+    padding: 12px 24px;
+  }
+}
+
+@media (min-width: 1281px) and (max-width: 1920px) {
+  .button-primary {
+    padding: 14px 28px;
+  }
+}
+
+@media (min-width: 1921px) {
+  .button-primary {
+    padding: 16px 32px;
+  }
+}
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="bg-accent-100">
+    <div class="container mx-auto px-4">
+      <br><br>
+      <ButtonPrimary to="/">Button test</ButtonPrimary>
+    </div>
     <TypographyChart />
   </div>
 </template>
