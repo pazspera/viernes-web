7aff44695c03025fe1e09b9ed71f1a97eb15ed56
Paz Spera <mspera03@gmail.com>
2026-06-01 01:00:40 -0300

    refactor: Actualizada imagen de Climax para que tenga la misma proporción que las otras cards. Agregado aspect-radio a la img a CardMovie

--- app/components/CardMovie.vue
+++ app/components/CardMovie.vue
@@ -7,7 +7,7 @@ const props = defineProps<{ movie: Movie }>();
 
 <template>
   <div class="col-span-12 lg:col-span-6 2xl:col-span-4 relative">
-    <img :src="props.movie.img_card" :alt="props.movie.img_card_alt" />
+    <img :src="props.movie.img_card" :alt="props.movie.img_card_alt" class="aspect-3/2" />
     <div class="bg-black text-white py-2 px-4">
       <CardHeading>
         <NuxtLink :to="`peliculas/${props.movie.link_page}`" class="after:absolute after:inset-0">
