82685eda306d577b449733ed7f67d4ce37248794
Paz Spera <mspera03@gmail.com>
2026-06-16 00:20:17 -0300

    style: Agregado estilo a YearCard

--- app/components/YearCard.vue
+++ app/components/YearCard.vue
@@ -9,8 +9,8 @@ const props = defineProps<{ historyYear: HistoryYear }>();
 </script>
 
 <template>
-  <div class="col-span-6 md:col-span-4">
-    <div v-if="props.historyYear.year">
+  <div class="col-span-6 md:col-span-4 year-card">
+    <div v-if="props.historyYear.year" class="bg-black text-white py-2 px-4">
       <CardHeading>{{ props.historyYear.year }}</CardHeading>
     </div>
     <NuxtLink :to="props.historyYear.to" target="_blank">
