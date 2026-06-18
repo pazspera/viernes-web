04843f83f360c8689b28238932e689a468d20cc4
Paz Spera <mspera03@gmail.com>
2026-06-18 00:14:30 -0300

    chore: Actualizadas imágenes restantes 2021 y parte de 2022

--- app/components/YearCard.vue
+++ app/components/YearCard.vue
@@ -9,7 +9,7 @@ const props = defineProps<{ historyYear: HistoryYear }>();
 </script>
 
 <template>
-  <div class="col-span-6 md:col-span-4 year-card">
+  <div class="col-span-6 md:col-span-4 year-card box-shadow-z1">
     <div v-if="props.historyYear.year" class="bg-black text-white py-2 px-4">
       <CardHeading>{{ props.historyYear.year }}</CardHeading>
     </div>
